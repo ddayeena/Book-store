3daa6a8ec14be61541c8d92ff35bf77c48e506b2
ddayeena <dianazdanevych@gmail.com>
2024-06-24 13:55:30 +0300

    add search for books in catalog

--- src/App.vue
+++ src/App.vue
@@ -126,6 +126,7 @@ nav a:hover {
   min-width: 160px;
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   border-radius: 20px;
+  z-index:1;
 }
 
 .dropdown-content a {
--- src/components/catalog/ThrillersAndHorror.vue
+++ src/components/catalog/ThrillersAndHorror.vue
@@ -3,35 +3,45 @@
     <h1>ТРИЛЕРИ ТА ЖАХИ</h1>
     <p class="start">Захоплюючі історії для тих, хто не проти полоскотати свої нерви. 
                     Психологічні, детективні та містичні трилери, хоррор, книги, за якими були зняті найкращі фільми жахів.</p>
-    <div v-if="books.length === 0">Завантаження...</div>
+
+    <div class="search-container">
+      <i class="fa fa-search search-icon"></i>
+      <input type="text" v-model="searchQuery" placeholder="Пошук книги" class="search-bar">
+    </div>
+
+    <div v-if="filteredBooks.length === 0" class="search-text">Не знайдено книг за запитом «{{searchQuery}}»</div>
 
     <div v-else>
-      <div class="book-row" v-for="(row, index) in Math.ceil(books.length / 4)" :key="index" >
-        <div class="book" v-for="book in getBooksInRow(index)" :key="book.id">
+      <div class="book-row" v-for="(row, index) in Math.ceil(filteredBooks.length / 4)" :key="index">
+        <div class="book" v-for="book in getBooksInRow(index, filteredBooks)" :key="book.id">
+
           <router-link :to="{ name: 'Book', params: { id: book.id }}">
             <img :src="book.img_src" alt="Book Cover" class="book-cover">
           </router-link>
+
           <div class="book-info">
-            <p  class = "price"><b>{{ book.price }}</b> грн</p>
-              <router-link :to="{ name: 'Book', params: { id: book.id }}">
-                <h3 class = "name">{{ book.name }}</h3>
-              </router-link>
-            <p class = "author">{{ book.author }}</p>
+            <p class="price"><b>{{ book.price }}</b> грн</p>
+            <router-link :to="{ name: 'Book', params: { id: book.id }}">
+              <h3 class="name">{{ book.name }}</h3>
+            </router-link>
+            <p class="author">{{ book.author }}</p>
           </div>
+
         </div>
       </div>
     </div>
   </div>
 </template>
 
+
 <script>
 import axios from 'axios';
-
 
 export default {
   data() {
     return {
-      books: []
+      books: [],
+      searchQuery: ''
     };
   },
   created() {
@@ -47,15 +57,29 @@ export default {
           console.error("There was an error!", error);
         });
     },
-    getBooksInRow(rowIndex) {
+    getBooksInRow(rowIndex, books) {
       const start = rowIndex * 4;
       const end = start + 4;
-      return this.books.slice(start, end);
+      return books.slice(start, end);
+    }
+  },
+  computed: {
+    filteredBooks() {
+      if (!this.searchQuery) {
+        return this.books;
+      }
+      const searchQueryLower = this.searchQuery.toLowerCase();
+      return this.books.filter(book => {
+        const nameMatches = book.name.toLowerCase().includes(searchQueryLower);
+        const authorMatches = book.author.toLowerCase().includes(searchQueryLower);
+        return nameMatches || authorMatches;
+      });
     }
   }
-}
+};
 </script>
 
 <style scoped>
 @import url('@/assets/css/bookstyle.css');
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'); 
 </style>
